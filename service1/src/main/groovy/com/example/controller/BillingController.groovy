b77c3fd6e79f666dc509a858347094dae119a2fb
xinqiz <xzhang198@dons.usfca.edu>
2021-04-01 23:12:31 -0700

    assignment11

--- service1/src/main/groovy/com/example/controller/BillingController.groovy
+++ service1/src/main/groovy/com/example/controller/BillingController.groovy
@@ -1,21 +1,24 @@
 package com.example.controller
 
-import com.example.Service2Client
+//import com.example.Service2Client
+import com.example.ServiceClient
 import com.example.domain.Book
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.retry.annotation.Recoverable
 import io.reactivex.Flowable
 
 @Controller("/api/billing")
+@Recoverable
 class BillingController {
-    Service2Client client
+    ServiceClient client
 
-    BillingController(Service2Client client){
+    BillingController(ServiceClient client){
         this.client = client
     }
 
     @Get
-    Flowable<ArrayList<Book>> index(){
+    Flowable<ArrayList<Book>> findBooks(){
         client.findBooks()
     }
 }
